4a932586e9a678026f552836d6f424f194168ad9
coochieslayer9000 <umarfarhan987@gmail.com>
2025-12-07 10:59:11 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home.jsx";
-import Watch from "./Watch.jsx";
+import Home from "./pages/Home.jsx";
+import Watch from "./pages/Watch.jsx";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-900 text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/watch/:id" element={<Watch />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
     </Router>
   );
 }
