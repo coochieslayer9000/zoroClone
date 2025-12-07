328ddce7136ed6ca16b9e329bb820a4c20f2083d
coochieslayer9000 <umarfarhan987@gmail.com>
2025-12-07 10:55:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Watch from "./pages/Watch.jsx";
+import Home from "./Home.jsx";
+import Watch from "./Watch.jsx";
 
 function App() {
   return (
